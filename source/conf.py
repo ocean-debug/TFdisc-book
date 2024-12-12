d41be6e907126432544ad59569969d0cf1561178
why <22111520044@m.fudan.edu.cn>
2024-12-12 12:40:41 +0800

    first commit

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+project = 'TFdisc'
+copyright = '2024, Haiyang Wang'
+author = 'Haiyang Wang'
+release = '1.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark',
+    'sphinx_markdown_tables']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'EN'
+
+
+
+
---
+++ source/index.rst
@@ -0,0 +1,19 @@
+.. TFdisc documentation master file, created by
+   sphinx-quickstart on Thu Dec 12 10:36:24 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to TFdisc's documentation!
+========================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   about
+
