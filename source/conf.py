fd9ff66aa5580198d3350c7b8cb4f411cf12f281
why <22111520044@m.fudan.edu.cn>
2024-12-13 11:15:16 +0800

    first commit

--- source/conf.py
+++ source/conf.py
@@ -6,6 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sphinx_rtd_theme
+import recommonmark
+from recommonmark.transform import AutoStructify
 
 html_theme = 'sphinx_rtd_theme'
 
@@ -27,11 +29,19 @@ extensions = ['recommonmark',
     'sphinx_markdown_tables']
 
 templates_path = ['_templates']
-source_suffix = ['.rst', '.md']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 exclude_patterns = []
 
 language = 'EN'
 
 
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'auto_toc_tree_section': 'Contents',
+    }, True)
+    app.add_transform(AutoStructify)
 
 
--- source/contact/index.md
+++ source/contact/index.md
@@ -4,12 +4,13 @@ For questions and help requests, you can reach out to WHY by email.
 
 If you found a bug, please post on [the Github repository issue page](https://github.com/ocean-debug/TFdisc/issues)
 
-### Zhu lab
+## Zhu lab
 Zhu Lab, Institutes of Brain Science, Fudan University, Shanghai 200032, China  
 
 The homepage of our lab is here: [Zhu Lab](https://zhuy-lab.fudan.edu.cn)
 
-E-mails:
+E-mails:  
+
 Lab PI: Ying Zhu <ying_zhu@fudan.edu.cn>
 
 code developer:WHY <22111520044@m.fudan.edu.cn>
